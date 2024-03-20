b8a8f4be438622c283cd338e18eb35a15ca64863
Roberto Ramirez <robert.624@hotmail.com>
2024-03-19 22:54:56 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>
+    Hello world!
+    <span class="active">Active</span>
+  </h1>
+</main>
+
+<style lang="scss" scoped>
+
+  @import 'vars.scss';
+
+  $custom-blue: #0000ff;
+
+  h1 {
+    color: $charcoal;
+    text-align: center;
+    span {
+      &.active {
+        color: $red;
+      }
+    }
+  }
+</style>
